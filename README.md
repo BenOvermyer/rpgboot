54e43e4d52e9737fddf217faeb911e1e26f100fc
Ben Overmyer <ben@overmyer.net>
2021-05-04 08:02:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# RPGBoot
+
+This is a script to bootstrap a new tabletop role-playing game project.
+
+Invoke it from the parent directory of your source code directories like so:
+
+```
+rpgboot/rpgboot.py mygame "My Game"
+```
+
+The first argument is the file slug, and the second is the game title.
---
+++ rpgboot.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from jinja2 import Environment, FileSystemLoader
+import os, sys
+
+slug = sys.argv[1]
+title = sys.argv[2]
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+file_loader = FileSystemLoader(BASE_DIR + '/templates')
+env = Environment(
+  block_start_string = '\BLOCK{',
+	block_end_string = '}',
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%%',
+	line_comment_prefix = '%#',
+	trim_blocks = True,
+	autoescape = False,
+  loader=file_loader
+)
+
+print(BASE_DIR)
+
+CURRENT_DIR = os.getcwd()
+PROJECT_DIR = CURRENT_DIR + '/' + slug
+
+os.mkdir(PROJECT_DIR)
+os.mkdir(PROJECT_DIR + '/manuscript')
+os.mkdir(PROJECT_DIR + '/images')
+
+filenames = [
+  '.editorconfig',
+  '.gitignore',
+  'gamebook.cls',
+]
+
+for filename in filenames:
+  template = env.get_template(filename + '.jinja')
+  output = template.render()
+  with open(PROJECT_DIR + '/' + filename, 'w') as file:
+    file.write(output)
+
+template = env.get_template('game.tex.jinja')
+output = template.render(title=title, slug=slug)
+with open(PROJECT_DIR + '/' + slug + '.tex', 'w') as file:
+  file.write(output)
